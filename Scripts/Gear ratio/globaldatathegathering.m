aa397e5e5dc23bcff6476107df51a889a613a58d
les4675 <113148372+les4675@users.noreply.github.com>
2023-02-04 12:13:01 -0500

    L+ Gear Ratio

---
+++ Scripts/Gear ratio/globaldatathegathering.m
@@ -0,0 +1,33 @@
+% Requires Mapping toolbox to run
+clear;
+
+[file,path] = uigetfile('*.geojson');
+if isequal(file,0)
+   disp('User selected Cancel');
+else
+   disp(['User selected ', fullfile(path,file)]);
+   
+end
+
+GT = readgeotable(fullfile(path,file));
+% geoplot(GT)   // Plots the path on a map
+d = 27;
+
+figure("Name","Motor Speed vs actual speed")
+hold on
+plot((GT.RPM_actual * d * pi * 60)/63360)
+plot(GT.speed)
+
+hold off
+figure("Name","Motor RPM")
+plot(GT.RPM_actual)
+
+Gearratio = sum((GT.RPM_actual * d * pi * 60)/63360)/sum(GT.speed);
+
+figure("Name", "Gear Ratio")
+vectGear = ((GT.RPM_actual * d * pi * 60)/63360)./GT.speed;
+plot(vectGear)
+
+maxmph = max(GT.speed);
+minmph = min(GT.speed);
+physicalgearratio = 52/12;
