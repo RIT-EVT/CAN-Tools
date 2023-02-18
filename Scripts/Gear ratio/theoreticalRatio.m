31c6d069b13a42f3e12f80699e4085b2bc01e3dd
les4675 <113148372+les4675@users.noreply.github.com>
2023-02-18 13:06:28 -0500

    :-1:

--- Scripts/Gear ratio/theoreticalRatio.m
+++ Scripts/Gear ratio/theoreticalRatio.m
@@ -11,6 +11,8 @@ end
 
 GT = readgeotable(fullfile(path,file));
 
+d = 27;
+
 currfrontSproket = 12;
 currbackSproket = 52;
 currentGearRatio = currbackSproket/currfrontSproket;
@@ -27,7 +29,15 @@ avgRPM = sum(RPM(2000:2500))/500;
 RPMscale = (1:5000);
 newRPMavg = avgRPM * newGearRatio / currentGearRatio;
 newRPMscale = RPMscale  .* newGearRatio ./ currentGearRatio;
+figure
+plot(RPMscale,RPMscale * d * pi * 60 /63360, 'r', RPMscale, (newRPMscale * d * pi * 60)/63360, 'b')
+xlabel('RPM')
+ylabel('Velocity (mph)')
+title('Projected RPM gain')
 
-hold on;
-plot(RPMscale)
-plot(newRPMscale)
+
+
+%figure
+
+
+comp = 5000 / (newGearRatio / currentGearRatio)
