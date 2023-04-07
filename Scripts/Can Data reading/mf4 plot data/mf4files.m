858c6e7ab9a76f27d6b497adae5178bed78538fc
les4675 <113148372+les4675@users.noreply.github.com>
2023-04-06 20:00:39 -0400

    THANKS MATT FOR MAKEING MORE WORK FOR ME :pout:

--- Scripts/Can Data reading/mf4 plot data/mf4files.m
+++ Scripts/Can Data reading/mf4 plot data/mf4files.m
@@ -29,6 +29,7 @@ N10_TPDO2 = canSignalTimetable(msgTimetable, "N10_TPDO2");
 N10_TPDO3 = canSignalTimetable(msgTimetable, "N10_TPDO3");
 
 N8_TPDO0 = canSignalTimetable(msgTimetable, "N8_TPDO0");
+N8_TPDO1 = canSignalTimetable(msgTimetable, "N8_TPDO1");
 
 
 
